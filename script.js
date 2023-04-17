91e58dde48fbc789d19ab3e8ce30a65f33373c8d
matesera <87706897+matesera@users.noreply.github.com>
2023-04-17 07:56:25 +0200

    Added hover effect on elements with event listeners

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 //set up a 16x16 grid of divs usig JavaScript
 
 //create 1 div with a class so that it can be styled by css
-for (let i = 0; i < 16; i++) {
-    const divContainer = document.querySelector('.container');
+const divContainer = document.querySelector('.container');
 
+
+for (let i = 0; i < 256; i++) {
     const divBlock = document.createElement('div');
     divBlock.classList.add('div-element');
     divContainer.appendChild(divBlock);
 }
+
+//set up an eventlistener so that when the mouse enters the div it changes color
+//we already have it styled in css, just need to add the class in the eventlistener
+const divElement = document.querySelector('.div-element');
+divElement.addEventListener('mouseenter', () => {
+    divElement.classList.add('div-element:hover');
+});
+
+divElement.addEventListener('mouseleave', () => {
+    divElement.classList.remove('div-element:hover');
+});
