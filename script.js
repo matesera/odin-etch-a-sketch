7247b9395ca5bf25e5bc60b8740ba1be375d3434
matesera <87706897+matesera@users.noreply.github.com>
2023-05-10 09:17:47 +0200

    changed the creation of the grid from flexbox to grid-layout and changed class names

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 //set up a 16x16 grid of divs usig JavaScript
 
 //create 1 div with a class so that it can be styled by css
-const divContainer = document.querySelector('.container');
+const divContainer = document.querySelector('.grid-container');
+divContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+divContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
 
 
 for (let i = 0; i < 256; i++) {
     const divBlock = document.createElement('div');
-    divBlock.classList.add('div-element');
+    divBlock.classList.add('grid-square');
     divContainer.appendChild(divBlock);
 }
 
 //set up an eventlistener so that when the mouse enters the div it changes color
 //we already have it styled in css, just need to add the class in the eventlistener
-const divElement = document.querySelector('.div-element');
+const divElement = document.querySelector('.grid-square');
 divElement.addEventListener('mouseenter', () => {
-    divElement.classList.add('div-element:hover');
+    divElement.classList.add('grid-square:hover');
 });
 
 divElement.addEventListener('mouseleave', () => {
-    divElement.classList.remove('div-element:hover');
+    divElement.classList.remove('grid-square:hover');
 });
+
+//make it so that when you hover over a div element it doesn't change immediately to grey, but slighty becomes more dark
+
+//rgb transition steps
+
+//start = rgb(255, 255, 255) //white
+//transition1 = rgb(242, 242, 242)
+//transition2 = rgb(230, 230, 230) 
+//transition3 = rgb(217, 217, 217)
+//transition4 = rgb(204, 204, 204)
+//transition5 = rgb(191, 191, 191)
+//transition6 = rgb(179, 179, 179)
+//transition7 = rgb(166, 166, 166)
+//transition8 = rgb(153, 153, 153)
+//transition9 = rgb(140, 140, 140)
+//end = rgb(128, 128, 128) // grey
